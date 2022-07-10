736982c6ac91cc35e8d8f5ebe74b42e7a8416a1d
Seonggu Kim <seonggu.de.kim@gmail.com>
2022-07-10 18:01:53 +0900

    update

---
+++ src/Config.ts
@@ -0,0 +1,20 @@
+export const WEDDING_INVITATION_URL = 'http://localhost:3000';
+export const KAKAOTALK_API_TOKEN = 'c47ecae77336131a752421a673c83445';
+export const KAKAOTALK_SHARE_IMAGE = '카카오톡으로 공유할 때 사용되는 대표이미지 URL을 입력해주세요!';
+
+export const WEDDING_DATE = '2022년 10월 15일, 토요일 오전 12시 00분';
+export const WEDDING_LOCATION = '로얄호텔 서울, 2층 뿡빵홀';
+
+export const GROOM_NAME = '김성구';
+export const GROOM_ACCOUNT_NUMBER = '○○은행 ***-***-******';
+export const GROOM_FATHER_NAME = '김주선';
+export const GROOM_FATHER_ACCOUNT_NUMBER = '○○은행 ***-***-******';
+export const GROOM_MOTHER_NAME = '서란희';
+export const GROOM_MOTHER_ACCOUNT_NUMBER = '○○은행 ***-***-******';
+
+export const BRIDE_NAME = '이예지';
+export const BRIDE_ACCOUNT_NUMBER = '○○은행 ***-***-******';
+export const BRIDE_FATHER_NAME = '이강희';
+export const BRIDE_FATHER_ACCOUNT_NUMBER = '○○은행 ***-***-******';
+export const BRIDE_MOTHER_NAME = '한영순';
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = '○○은행 ***-***-******';
