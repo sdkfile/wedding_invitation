1826c206cfec0b0aaa9c10f8a90f2c1d6d4de367
Seonggu Kim <seonggu.de.kim@gmail.com>
2022-07-24 14:57:43 +0900

    updated some

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,4 @@
-export const WEDDING_INVITATION_URL = 'http://localhost:3000';
+export const WEDDING_INVITATION_URL = 'https://sdkfile.github.io/wedding_invitation/';
 export const KAKAOTALK_API_TOKEN = 'c47ecae77336131a752421a673c83445';
 export const KAKAOTALK_SHARE_IMAGE = 'https://github.com/sdkfile/wedding_invitation/blob/main/src/Assets/Gallery_Photo_2.jpg?raw=true';
 
@@ -8,13 +8,13 @@ export const WEDDING_LOCATION = '로얄호텔 서울, 3층 그랜드볼룸';
 export const GROOM_NAME = '김성구';
 export const GROOM_ACCOUNT_NUMBER = '카카오뱅크 3333-08-2951741';
 export const GROOM_FATHER_NAME = '김주선';
-export const GROOM_FATHER_ACCOUNT_NUMBER = '○○은행 ***-***-******';
+export const GROOM_FATHER_ACCOUNT_NUMBER = '하나은행 332-910175-45707';
 export const GROOM_MOTHER_NAME = '서란희';
-export const GROOM_MOTHER_ACCOUNT_NUMBER = '○○은행 ***-***-******';
+export const GROOM_MOTHER_ACCOUNT_NUMBER = '국민은행 496501-01-032893';
 
 export const BRIDE_NAME = '이예지';
-export const BRIDE_ACCOUNT_NUMBER = '○○은행 ***-***-******';
+export const BRIDE_ACCOUNT_NUMBER = '신한은행 110-163-720600';
 export const BRIDE_FATHER_NAME = '이강희';
-export const BRIDE_FATHER_ACCOUNT_NUMBER = '○○은행 ***-***-******';
+export const BRIDE_FATHER_ACCOUNT_NUMBER = '신한은행 110-451-070788';
 export const BRIDE_MOTHER_NAME = '한영순';
-export const BRIDE_MOTHER_ACCOUNT_NUMBER = '○○은행 ***-***-******';
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = '신한은행 110-272-820159';
