cdee7b704d6ef85e154826940d27f209041bceca
Seonggu Kim <seonggu.de.kim@gmail.com>
2022-07-24 16:35:53 +0900

    changed font and check icon

--- src/Components/BusInfo.tsx
+++ src/Components/BusInfo.tsx
@@ -21,7 +21,7 @@ export default function BusInfo() {
         <em>
           <a
             href="https://map.kakao.com/?map_type=TYPE_MAP&amp;target=walk&amp;rt=497256%2C1129289%2C496820%2C1129091&amp;rt1=%EB%82%A8%EB%8C%80%EB%AC%B8%EC%84%B8%EB%AC%B4%EC%84%9C.%EC%84%9C%EC%9A%B8%EB%B0%B1%EB%B3%91%EC%9B%90&amp;rt2=%EB%A1%9C%EC%96%84%ED%98%B8%ED%85%94%EC%84%9C%EC%9A%B8&amp;rtIds=%2C&amp;rtTypes=%2C"
-          >남대문세무서.서울백병원</a> (도보&nbsp;4분)
+          >남대문세무서.서울백병원</a> (도보 4분)
         </em>
       </li>
       <li>
@@ -52,7 +52,7 @@ export default function BusInfo() {
       <li>
         <em className="point"><a
           href="https://map.kakao.com/?map_type=TYPE_MAP&amp;target=walk&amp;rt=497294%2C1129368%2C496820%2C1129091&amp;rt1=%EB%82%A8%EB%8C%80%EB%AC%B8%EC%84%B8%EB%AC%B4%EC%84%9C.%EA%B5%AD%EA%B0%80%EC%9D%B8%EA%B6%8C%EC%9C%84%EC%9B%90%ED%9A%8C&amp;rt2=%EB%A1%9C%EC%96%84%ED%98%B8%ED%85%94%EC%84%9C%EC%9A%B8&amp;rtIds=%2C&amp;rtTypes=%2C7"
-        >남대문세무서.국가인권위원회</a> (도보&nbsp;4분) </em>
+        >남대문세무서.국가인권위원회</a> (도보 4분) </em>
       </li>
       <li>
         <TableBus>
@@ -72,7 +72,7 @@ export default function BusInfo() {
       <li>
         <em className="point"><a
           href="https://map.kakao.com/?map_type=TYPE_MAP&amp;target=walk&amp;rt=497301%2C1129100%2C496820%2C1129091&amp;rt1=%EC%84%9C%EC%9A%B8%EB%B0%B1%EB%B3%91%EC%9B%90.%EA%B5%AD%EA%B0%80%EC%9D%B8%EA%B6%8C%EC%9C%84.%EC%95%88%EC%A4%91%EA%B7%BC%ED%99%9C%EB%8F%99%ED%84%B0&amp;rt2=%EB%A1%9C%EC%96%84%ED%98%B8%ED%85%94%EC%84%9C%EC%9A%B8&amp;rtIds=%2C&amp;rtTypes=%2C"
-        >서울백병원.국가인권위.안중근활동터</a> (도보&nbsp;4분) </em>
+        >서울백병원.국가인권위.안중근활동터</a> (도보 4분) </em>
       </li>
       <li>
         <TableBus>
@@ -101,7 +101,7 @@ export default function BusInfo() {
         <em>
           <a
             href="https://map.kakao.com/?map_type=TYPE_MAP&amp;target=walk&amp;rt=496756%2C1129573%2C496820%2C1129091&amp;rt1=%EC%84%9C%EC%9A%B8%EB%B0%B1%EB%B3%91%EC%9B%90.%EC%9D%84%EC%A7%80%EB%A1%9C%EC%9E%85%EA%B5%AC.%EB%A1%9C%EC%96%84%ED%98%B8%ED%85%94&amp;rt2=%EB%A1%9C%EC%96%84%ED%98%B8%ED%85%94%EC%84%9C%EC%9A%B8&amp;rtIds=%2C&amp;rtTypes=%2C"
-          >을지로입구.로얄호텔</a> (도보&nbsp;5분)
+          >을지로입구.로얄호텔</a> (도보 5분)
         </em>
       </li>
       <li>
@@ -122,7 +122,7 @@ export default function BusInfo() {
       <li>
         <em className="point"><a
           href="https://map.kakao.com/?map_type=TYPE_MAP&amp;target=walk&amp;rt=496156%2C1129210%2C496820%2C1129091&amp;rt1=%EB%AA%85%EB%8F%99%EA%B5%AD%EB%AF%BC%EC%9D%80%ED%96%89%EC%95%9E&amp;rt2=%EB%A1%9C%EC%96%84%ED%98%B8%ED%85%94%EC%84%9C%EC%9A%B8&amp;rtIds=%2C&amp;rtTypes=%2C"
-        >명동국민은행 앞</a> (도보&nbsp;5분)</em>
+        >명동국민은행 앞</a> (도보 5분)</em>
       </li>
       <li>
         <TableBus>
