7124da447cceb6eb62b4332b1925bb856c56010f
Seonggu Kim <seonggu.de.kim@gmail.com>
2022-08-30 22:17:09 +0900

    fixed video size and map draggable issue

--- src/Components/KakaoMapScript.tsx
+++ src/Components/KakaoMapScript.tsx
@@ -8,12 +8,6 @@ export default function KakaoMapScript() {
   };
   let map = new kakao.maps.Map(container, options);
 
-  // const markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667);
-  // let marker = new kakao.maps.Marker({
-  //     position: markerPosition
-  // });
-  // marker.setMap(map);
-
   // 주소-좌표 변환 객체를 생성합니다
   const geocoder = new kakao.maps.services.Geocoder();
 
@@ -30,15 +24,17 @@ export default function KakaoMapScript() {
       });
 
       // 인포윈도우로 장소에 대한 설명을 표시합니다
-      const infowindow = new kakao.maps.InfoWindow({
+      const infoWindow = new kakao.maps.InfoWindow({
         content: `<div style="width:150px;text-align:center;padding:6px 0;"><a href="https://place.map.kakao.com/24633671">로얄호텔 서울</a></div>`,
       });
-      infowindow.open(map, marker);
+      infoWindow.open(map, marker);
 
       // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
       map.setCenter(coords);
     }
   });
-  //줌 막기
+  // 줌 막기
   map.setZoomable(false);
+  // 드래그 막기
+  map.setDraggable(false);
 }
